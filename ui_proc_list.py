a70ab02eb52f7205f09913d21b4dabd5a095f4f6
Christopher Hewitt <hewittc@users.noreply.github.com>
2015-01-20 08:56:45 -0500

    Migrating work from laptop.

--- ui_proc_list.py
+++ ui_proc_list.py
@@ -30,49 +30,56 @@ class MemViewUIProcListWindow(Gtk.Window):
         self.set_size_request(600, 600)
         self.connect('destroy', Gtk.main_quit)
 
-        self.process_liststore = Gtk.ListStore(int, str, str, int, int, int)
-        self.renderer_text = Gtk.CellRendererText()
+        self.process_treeview_scroller = self.create_process_list()
+        self.ptrace_status_label = Gtk.Label('', xalign=1)
 
-        self.vbox = Gtk.VBox()
-
-        self.process_treeview_scroller = Gtk.ScrolledWindow()
-        self.process_treeview_scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        self.process_treeview = Gtk.TreeView(model=self.process_liststore)
-
-        self.column_pid_text = Gtk.TreeViewColumn('PID', self.renderer_text, text=0)
-        self.column_filename_text = Gtk.TreeViewColumn('Filename', self.renderer_text, text=1)
-        self.column_state_text = Gtk.TreeViewColumn('State', self.renderer_text, text=2)
-        self.column_ppid_text = Gtk.TreeViewColumn('Parent PID', self.renderer_text, text=3)
-        self.column_pgrp_text = Gtk.TreeViewColumn('Process GID', self.renderer_text, text=4)
-        self.column_session_text = Gtk.TreeViewColumn('Session ID', self.renderer_text, text=5)
-
-
-        self.process_treeview.append_column(self.column_pid_text)
-        self.process_treeview.append_column(self.column_filename_text)
-        self.process_treeview.append_column(self.column_state_text)
-        self.process_treeview.append_column(self.column_ppid_text)
-        self.process_treeview.append_column(self.column_pgrp_text)
-        self.process_treeview.append_column(self.column_session_text)
-
-
-        self.process_treeview_scroller.add(self.process_treeview)
-
-        self.ptrace_status_label = Gtk.Label('None', xalign=1)
-
-        self.vbox.pack_start(self.process_treeview_scroller, True, True, 0)
-        self.vbox.pack_start(self.ptrace_status_label, False, True, 0)
-
-        self.add(self.vbox)
+        vbox = Gtk.VBox()
+        vbox.pack_start(self.process_treeview_scroller, True, True, 0)
+        vbox.pack_start(self.ptrace_status_label, False, True, 0)
+        self.add(vbox)
 
         self.show_all()
 
-    def update_ptrace_label(self, msg):
-        self.ptrace_status_label.set_text('Process trace restrictions: {msg}'.format(msg=msg))
+    def create_menu_bar(self):
+        action_group = Gtk.ActionGroup("goldfish_actions")
 
-    def update_process_liststore(self, processes):
+        self.add_file_menu_actions(action_group)
+        self.add_help_menu_actions(action_group)
+
+    def create_process_list(self):
+        self.process_liststore = Gtk.ListStore(int, str, str, int, int, int)
+
+        renderer_text = Gtk.CellRendererText()
+
+        process_treeview_scroller = Gtk.ScrolledWindow()
+        process_treeview_scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        process_treeview = Gtk.TreeView(model=self.process_liststore)
+
+        column_pid_text = Gtk.TreeViewColumn('PID', renderer_text, text=0)
+        column_filename_text = Gtk.TreeViewColumn('Filename', renderer_text, text=1)
+        column_state_text = Gtk.TreeViewColumn('State', renderer_text, text=2)
+        column_ppid_text = Gtk.TreeViewColumn('Parent PID', renderer_text, text=3)
+        column_pgrp_text = Gtk.TreeViewColumn('Process GID', renderer_text, text=4)
+        column_session_text = Gtk.TreeViewColumn('Session ID', renderer_text, text=5)
+
+        process_treeview.append_column(column_pid_text)
+        process_treeview.append_column(column_filename_text)
+        process_treeview.append_column(column_state_text)
+        process_treeview.append_column(column_ppid_text)
+        process_treeview.append_column(column_pgrp_text)
+        process_treeview.append_column(column_session_text)
+
+        process_treeview_scroller.add(process_treeview)
+
+        return process_treeview_scroller
+
+    def update_process_list(self, processes):
         self.process_liststore.clear()
         for process in processes:
             self.process_liststore.append([process['pid'], process['comm'], process['state'], process['ppid'], process['pgrp'], process['session']])
+
+    def update_ptrace_label(self, msg):
+        self.ptrace_status_label.set_text('Process trace restrictions: {msg}'.format(msg=msg))
 
     def run(self):
         Gtk.main()
